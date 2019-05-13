00076cd2fbabf02347d953ce079ec89ba41333d3
Senistan <senistan.jegarajasingam@cpnv.ch>
2019-05-13 14:59:50 +0200

    First commit

---
+++ src/providers/data.ts
@@ -0,0 +1,27 @@
+export class DataProvider {
+    public receipes = [
+        "id": "tarte-citron-meringue",
+        "name": "Tarte citron meringué",
+        "creatorId": 1,
+        "isAvailable": true,
+        "picture": "img/recettes/tarte-citron-meringue.jpg",
+        "calories": 460,
+        "ingredients": [
+            "oeuf",
+            "citron",
+            "sucre",
+            "farine"
+            ],
+    ]
+
+    public init(){
+
+    }
+
+    public store(){
+
+    }
+    public reload(){
+
+    }
+}
